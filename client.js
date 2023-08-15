febd43471ea4a9456dd77e508aff549378aa235d
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-15 14:35:34 -0500

    we are getting somewhere

--- client.js
+++ client.js
@@ -32,26 +32,74 @@ const employees = [
   }
 ];
 
-console.log('array of employee data: ',  employees );
+console.log('array of employee data: ', employees);
 
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
-// This problem is massive! Break the problem down, take small steps, and test as you go.
-// What is the fewest lines of code I can write and test to get just a little closer?
+// This problem is massive! 
+// Break the problem down, take small steps, and test as you go.
+// What is the fewest lines of code I can write and test 
+// to get just a little closer?
 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+//how do we calculate an employee bonus?
+// whats the function?
+// starting small!
+// // atticus's info:
+// {
+//   name: 'Atticus',
+//   employeeNumber: '2405',
+//   annualSalary: '47000',
+//   reviewRating: 3
+// },
 
-
+// this has two components
+// what is the rating
+// what is the bonus calculation (did they earn it?)
+// for atticus, his rating is 3 so his bonus is 4% of 47,000.00 
+// the total would be $1,880.00
 
 // This function will calculate 1 employee's bonus!
-//
-function calculateIndividualEmployeeBonus( employee ) {  
-  // your logic here
-  
-  
-  // return new object with bonus results
 
+
+function calculateIndividualEmployeeBonus(staffName) {
+  // your logic here
+  for (let aEmployee of employees) {
+
+    console.log(aEmployee.name);
+
+    let bonusPercentage = 0;
+    let totalBonus = aEmployee.annualSalary * bonusPercentage;
+    let totalCompensation = aEmployee.annualSalary + totalBonus;
+    for (i = 0; i < aEmployee.length; i++) {
+
+      if (aEmployee.name === staffName && aEmployee.reviewRating === 3) {
+        bonusPercentage = 0.04;
+      }
+    }
+    console.log(totalCompensation);
+    console.log(totalBonus);
+  }
+
+  // return new object with bonus results
 }
+
+console.log(calculateIndividualEmployeeBonus("Atticus"));
+// console.log(calculateIndividualEmployeeBonus("Atticus"));
+
+  // employees[0]
+  // console.log(employees[0]);
+
+  // if (employees.name === "atticus" && employees.reviewRating === 3) {
+  //   return employees.annualSalary *= 0.04
+
+  // }
+
+// we need to run the function with just atticus first to see if it works
+
+// employee.name= atticus
+// review rating= 3
+// then {}
